2993f201fdaa17cf8c5637c5363aa08c1dca8340
Samir1925 <95024897+Samir1925@users.noreply.github.com>
2023-02-13 11:21:42 +0545

    Add files via upload

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta content='width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0' name='viewport' />
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<link rel="stylesheet" href="https://www.tataplay.ml/css/style2.css">
+<link rel="stylesheet" href="https://www.tataplay.ml/css/styler.css">
+<script async="true" src="css/navbar2.js"></script>
+<script async="true" src="https://www.tataplay.ml/css/iconify.min.js"></script>
+<link href="https://fonts.googleapis.com/css2?family=PT+Sans&display=swap" rel="stylesheet">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.2/jquery.modal.min.js"></script>
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.css" /></head>
+<header style="display:fixed" class="header" id="header"><center>
+<a id="head-logo-href">
+<img src="https://i.postimg.cc/4dDRTSN2/20230211-183214.png" style="margin:2px;" alt="Compart Logo" itemprop="thumbnail" width="210px" height="auto"></a></center>
+<button class="bt2" onclick="loadMore()" aria-label="Search"><span>
+<svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="40" height="40" preserveAspectRatio="xMidYMid meet" viewBox="2.3 2.4 18 18">
+<g fill="currentColor">
+<path d="M9 9a2 2 0 1 1 4 0a2 2 0 0 1-4 0Z" />
+<path fill-rule="evenodd" d="M10 18a8 8 0 1 0 0-16a8 8 0 0 0 0 16Zm1-13a4 4 0 0 0-3.446 6.032l-2.261 2.26a1 1 0 1 0 1.414 1.415l2.261-2.261A4 4 0 1 0 11 5Z" clip-rule="evenodd" /></g></svg></span></button></header>
+<div style="background:transparent;height:64px;height:var(--header-height);"></div>
+<div id="search" style="display:none;background:transparent;z-index:3;position: sticky;top:64px;">
+ <input style="position: sticky;top:64px;background:transparent;color:brown;width:100%;z-index:3;margin-bottom:10px" class="fin" placeholder="Search channels here.." id="search" type="text"><hr>
+</div>
+<script> const search = document.getElementById('search');
+function loadMore() { if (search.style.display === 'none') { search.style.display = 'block'; } else { search.style.display = 'none'; } } </script>
+<link rel="stylesheet" href='https://www.tataplay.ml/css/loading.css'>
+<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/flickity/1.0.0/flickity.css'><style> .alignleft { float: left; margin-left: 3px; margin-top: 0px; color: black; font-weight: bold; font-size: 20px; }
+.alignleftt { float: left; margin-left: 15px; color: red; font-weight: bold; font-size: 20px; }
+.alignright { float: right; margin-right: 10px; color: white; margin-top: 9px; text-align: center; line-height: 130%; border-radius: 4px; width: 17%; background-color: #FF0000; font-size: 20px; font-weight: 10px; }
+h4 { margin-left: 5px; color: silver; font-weight: bold; font-size: 15px; }
+.nta { position: relative; overflow: hidden; }
+.slide-progress-bar { width: 99%; font-weight: 1000; text-align: center; background-color: black; transition: width 4s linear; display: inline-block; vertical-align: middle; }
+.progress-bar { height: 40px; background-color: green; background: linear-gradient(180deg, white, silver); font-weight: 1000; text-align: center; width: 1%; position: relative; transition: linear; }</style><body><script> var elem = document.getElementById("progress-bar"); var width = 1; var interval;
+function progressBar() { resetProgressBar(); interval = setInterval(frame, 100);
+function frame() { if (width >= 100) { clearInterval(interval); } else { width++; elem.style.width = width + '%'; } } }
+function resetProgressBar() { width = 1; clearInterval(interval) elem.style.width = width + '%'; } progressBar(); $('.progress-bar').animate({ width: '100%' }, { duration: 5000, step: function(now, fx) { if (fx.prop == 'width') { $(this).html(Math.round(now * 100) / 100 + '%'); } } }); </script><center>
+<a href="zee5"><div class="tvv-item"><img src="https://upload.wikimedia.org/wikipedia/en/thumb/3/30/Zee5_Official_logo.svg/1200px-Zee5_Official_logo.svg.png" class="loading" style="height:90%"><div class="tvv-name">ZEE-5</div></div></a>
+<a href="sonyliv"><div class="tvv-item"><img src="https://play-lh.googleusercontent.com/QtxELma_6y1jezI8QKqVtI8Tb0flMhPjIAzU-VSZ2jz7RwriCENXQk4M6MvUyVi5qg" class="loading" style="height:90%"><div class="tvv-name">SONY LIV</div></div></a>
+<a href="jio1"><div class="tvv-item"><img src="https://upload.wikimedia.org/wikipedia/commons/5/52/JioTV_logo.png" class="loading" style="height:90%"><div class="tvv-name">JIO TV</div></div></a>
+<a href="sunnxt"><div class="tvv-item"><img src="https://play-lh.googleusercontent.com/jFi2iC10wQJ42gu-DO2CMeIcN3qcmNQHtY5EBT_wtp4jCIozS4n3Q9pA7ZloDUGHHw" class="loading" style="height:90%"><div class="tvv-name">SUN NXT</div></div></a>
+<a href="Discovery+"><div class="tvv-item"><img src="https://upload.wikimedia.org/wikipedia/commons/d/d1/2016_Discovery_logo.svg" class="loading" style="height:90%"><div class="tvv-name">DISCOVERY+</div></div></a>
+<a href="stream"><div class="tvv-item"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Microsoft_Stream.svg/1200px-Microsoft_Stream.svg.png" class="loading" style="height:90%"><div class="tvv-name">NETWORK STREAM</div></div></a>
+</center>
+</body></html>
